6581ed8391b0ceb8216f7ee3362a20dd3d93896d
BoTranVan <tranbo9xnd@gmail.com>
2018-04-06 11:45:10 +0700

    Update prefiefix views

--- cachem/bin/database.py
+++ cachem/bin/database.py
@@ -1,20 +1,3 @@
 #!/usr/bin/env python3
 # -*- encoding: utf-8 -*-
 # @author: ministry
-try:
-    import psycopg2
-except ImportError as e:
-    raise e
-
-    
-
-__defautlConfig__ = {
-    "host": "localhost",
-    "port": "5432",
-    "database": "postgres",
-    "user": "postgres",
-    "password": "postgres"
-}
-
-def _connectDatabase():
-    conn = psycopg2.connect()
